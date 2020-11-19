01ee8b027e1e01437ca42c8ec7dc58e62908fe7b
Norris <norris@enclave.vn>
2020-11-19 11:21:09 +0700

    Change to mongodb db

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,8 @@
 FROM python:3.8-alpine
-
+RUN apk add --no-cache bash
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
-
-RUN apk add --update --no-cache postgresql-client
-RUN apk add --update --no-cache --virtual .tmp-build-deps \
-        gcc libc-dev linux-headers postgresql-dev
-
 RUN pip install -r /requirements.txt
 
 RUN mkdir /app
--- app/app/settings.py
+++ app/app/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
+        'ENGINE': 'djongo',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'CLIENT': {
+            'host': os.environ.get('DB_HOST'),
+            'username': os.environ.get('DB_USER'),
+            'password': os.environ.get('DB_PASS'),
+        }
     }
 }
 
