d8c9091227aadbfbd347a62d45a3f14d0e524ef4
Norris <norris@enclave.vn>
2020-11-24 11:10:29 +0700

    Added create ingredient feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -23,7 +23,7 @@ class TagViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(viewsets.ModelViewSet):
     """Manage ingredient is the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -33,3 +33,7 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
